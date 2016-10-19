a62b028834437e43b93319a2b54a7eea8af5023c
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2016-10-19 16:39:36 -0400

    complete instrument list to match scratch 2.0

--- src/index.js
+++ src/index.js
@@ -40,7 +40,9 @@ function AudioEngine (sounds) {
     // match scratch instruments
     this.instrumentNames = ['acoustic_grand_piano', 'electric_piano_1',
         'drawbar_organ', 'acoustic_guitar_nylon', 'electric_guitar_clean',
-         'acoustic_bass', 'pizzicato_strings', 'cello', 'trombone', 'clarinet'];
+         'acoustic_bass', 'pizzicato_strings', 'cello', 'trombone', 'clarinet',
+         'tenor_sax', 'flute', 'pan_flute', 'bassoon', 'choir_aahs', 'vibraphone',
+         'music_box', 'steel_drums', 'marimba', 'lead_1_square', 'fx_4_atmosphere'];
 
     this.setInstrument(0);
 
