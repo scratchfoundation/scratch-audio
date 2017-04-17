9874cf006f1d5d5b29933e68f4dd88a700839a73
Ray Schamp <ray@scratch.mit.edu>
2017-04-17 12:55:09 -0400

    Update to use ES6 class syntax

--- src/effects/PanEffect.js
+++ src/effects/PanEffect.js
@@ -5,49 +5,42 @@ const Tone = require('tone');
 * Effect value of -100 puts the audio entirely on the left channel,
 * 0 centers it, 100 puts it on the right.
 * Clamped -100 to 100
-* @constructor
 */
-const PanEffect = function () {
-    Tone.Effect.call(this);
+class PanEffect extends Tone.Effect {
+    constructor () {
+        super();
+        this.value = 0;
+        this.panner = new Tone.Panner();
+        this.effectSend.chain(this.panner, this.effectReturn);
+    }
 
-    this.value = 0;
+    /**
+    * Set the effect value
+    * @param {number} val - the new value to set the effect to
+    */
+    set (val) {
+        this.value = this.clamp(val, -100, 100);
+        this.panner.pan.value = this.value / 100;
+    }
 
-    this.panner = new Tone.Panner();
+    /**
+    * Change the effect value
+    * @param {number} val - the value to change the effect by
+    */
+    changeBy (val) {
+        this.set(this.value + val);
+    }
 
-    this.effectSend.chain(this.panner, this.effectReturn);
-};
-
-Tone.extend(PanEffect, Tone.Effect);
-
-/**
-* Set the effect value
-* @param {number} val - the new value to set the effect to
-*/
-PanEffect.prototype.set = function (val) {
-    this.value = val;
-
-    this.value = this.clamp(this.value, -100, 100);
-
-    this.panner.pan.value = this.value / 100;
-};
-
-/**
-* Change the effect value
-* @param {number} val - the value to change the effect by
-*/
-PanEffect.prototype.changeBy = function (val) {
-    this.set(this.value + val);
-};
-
-/**
-* Clamp the input to a range
-* @param {number} input - the input to clamp
-* @param {number} min - the min value to clamp to
-* @param {number} max - the max value to clamp to
-* @return {number} the clamped value
-*/
-PanEffect.prototype.clamp = function (input, min, max) {
-    return Math.min(Math.max(input, min), max);
-};
+    /**
+    * Clamp the input to a range
+    * @param {number} input - the input to clamp
+    * @param {number} min - the min value to clamp to
+    * @param {number} max - the max value to clamp to
+    * @return {number} the clamped value
+    */
+    clamp (input, min, max) {
+        return Math.min(Math.max(input, min), max);
+    }
+}
 
 module.exports = PanEffect;
--- src/effects/RoboticEffect.js
+++ src/effects/RoboticEffect.js
@@ -9,59 +9,58 @@ const Tone = require('tone');
 * Other values change the pitch of the effect, in units of 10 steps per semitone.
 * The effect value is not clamped (but probably should be).
 * Exterminate.
-* @constructor
 */
-const RoboticEffect = function () {
-    Tone.Effect.call(this);
+class RoboticEffect extends Tone.Effect {
+    constructor () {
+        super();
 
-    this.value = 0;
+        this.value = 0;
 
-    const time = this._delayTimeForValue(100);
-    this.feedbackCombFilter = new Tone.FeedbackCombFilter(time, 0.9);
+        const time = this._delayTimeForValue(100);
+        this.feedbackCombFilter = new Tone.FeedbackCombFilter(time, 0.9);
 
-    this.effectSend.chain(this.feedbackCombFilter, this.effectReturn);
-};
-
-Tone.extend(RoboticEffect, Tone.Effect);
-
-/**
-* Set the effect value
-* @param {number} val - the new value to set the effect to
-*/
-RoboticEffect.prototype.set = function (val) {
-    this.value = val;
-
-    // mute the effect if value is 0
-    if (this.value === 0) {
-        this.wet.value = 0;
-    } else {
-        this.wet.value = 1;
+        this.effectSend.chain(this.feedbackCombFilter, this.effectReturn);
     }
 
-    // set delay time using the value
-    const time = this._delayTimeForValue(this.value);
-    this.feedbackCombFilter.delayTime.rampTo(time, 1 / 60);
-};
+    /**
+    * Set the effect value
+    * @param {number} val - the new value to set the effect to
+    */
+    set (val) {
+        this.value = val;
 
-/**
-* Change the effect value
-* @param {number} val - the value to change the effect by
-*/
-RoboticEffect.prototype.changeBy = function (val) {
-    this.set(this.value + val);
-};
+        // mute the effect if value is 0
+        if (this.value === 0) {
+            this.wet.value = 0;
+        } else {
+            this.wet.value = 1;
+        }
 
-/**
-* Compute the delay time for an effect value.
-* Convert the effect value to a musical note (in units of 10 per semitone),
-* and return the period (single cycle duration) of the frequency of that note.
-* @param {number} val - the effect value
-* @returns {number} a delay time in seconds
-*/
-RoboticEffect.prototype._delayTimeForValue = function (val) {
-    const midiNote = ((val - 100) / 10) + 36;
-    const freq = Tone.Frequency(midiNote, 'midi').eval();
-    return 1 / freq;
-};
+        // set delay time using the value
+        const time = this._delayTimeForValue(this.value);
+        this.feedbackCombFilter.delayTime.rampTo(time, 1 / 60);
+    }
+
+    /**
+    * Change the effect value
+    * @param {number} val - the value to change the effect by
+    */
+    changeBy (val) {
+        this.set(this.value + val);
+    }
+
+    /**
+    * Compute the delay time for an effect value.
+    * Convert the effect value to a musical note (in units of 10 per semitone),
+    * and return the period (single cycle duration) of the frequency of that note.
+    * @param {number} val - the effect value
+    * @returns {number} a delay time in seconds
+    */
+    _delayTimeForValue (val) {
+        const midiNote = ((val - 100) / 10) + 36;
+        const freq = Tone.Frequency(midiNote, 'midi').eval();
+        return 1 / freq;
+    }
+}
 
 module.exports = RoboticEffect;
